2df1c1e715b39fd53abf1b29ef47af80681f3828
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2023-11-17 20:59:08 +0000

    Userspace page cache

--- src/IO/tests/gtest_writebuffer_s3.cpp
+++ src/IO/tests/gtest_writebuffer_s3.cpp
@@ -1240,8 +1240,7 @@ TEST_F(WBS3Test, ReadBeyondLastOffset) {
             std::move(rb_creator),
             objects,
             disk_read_settings,
-            cache_log,
-            true);
+            cache_log);
 
         auto rb_async = std::make_unique<AsynchronousBoundedReadBuffer>(
             std::move(rb_remote_fs), *reader, disk_read_settings, async_read_counters, prefetch_log);
